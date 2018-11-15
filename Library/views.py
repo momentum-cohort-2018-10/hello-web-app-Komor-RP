14accff7c4eeb7a6e13061d33f9655c99543df46
Fan Huang <fan.huang.33@gmail.com>
2018-11-15 07:48:09 -0500

    added some styling

--- Library/views.py
+++ Library/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from Library.forms import BookForm
 from Library.models import Book
 
 
@@ -14,4 +15,22 @@ def book_detail(request, slug):
 
     return render(request, 'books/book_detail.html', {
         'book': book,
+    })
+
+
+def edit_book(request, slug):
+    book = Book.objects.get(slug=slug)
+    form_class = BookForm
+    if request.method == 'POST':
+        form = form_class(data=request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_detail', slug=book.slug)
+
+    else:
+        form = form_class(instance=book)
+
+    return render(request, 'books/edit_book.html', {
+        'book': book,
+        'form': form,
     })
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from Library import views
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
     path('books/<slug>/', views.book_detail, name="book_detail"),
+    path('books/<slug>/edit', views.edit_book, name='edit_book'),
+    path('accounts/', include('registration.backends.simple.urls')),
     path('admin/', admin.site.urls),
 ]
